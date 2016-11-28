59e91661f96f06f4cfb2b8fc04c8e1a55d2401e7
Eldin <eldingraho@gmail.com>
2016-11-29 00:54:55 +0100

    fix typo

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var os = require('os');
-var router = express.Router();
 var fs = require('fs');
+var router = express.Router();
+
 
 /* GET api listing. */
 router.get('/', function(req, res, next) {
   res.send(os.cpus())
 });
 
-router.get('/on', functiono(req, res, next) {
+router.get('/on', function(req, res, next) {
   fs.writeFileSync('/sys/class/gpio/gpio4/value', 0);
-  res.send('on')
+  fs.writeFileSync()
 });
 
-router.get('/off', functiono(req, res, next) {
+router.get('/off', function(req, res, next) {
   fs.writeFileSync('/sys/class/gpio/gpio4/value', 0);
   res.send('off')
 });
